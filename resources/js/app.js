5b020714c281bf7876af5f78e0ff1d48c31e4afb
Whylgher Ferreira <whylgher@gmail.com>
2023-01-29 23:36:48 +0000

    add vuejs vitejs

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
+
+import { createApp } from 'vue';
 import './bootstrap';
+import ExampleComponent from './components/ExampleComponent.vue';
+
+
+const app = createApp({});
+
+app.component('example-component', ExampleComponent);
+
+app.mount('#app');
+
+// createApp(ExampleComponent).mount("#app");
