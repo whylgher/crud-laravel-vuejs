59aad0dd466301a31f977c9f9411543d026e49aa
Whylgher Ferreira <whylgher@gmail.com>
2023-01-31 08:37:28 +0000

    add Swal

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,20 @@
-import './bootstrap';
-
+import Swal from 'sweetalert2';
 import { createApp } from 'vue';
+import './bootstrap';
 
 import router from "./router/index";
 
 import App from "./components/App.vue";
 
-createApp(App).use(router).mount("#app")
+window.Swal = Swal;
+const toast = Swal.mixin({
+    toast: true,
+    position: 'center',
+    showConfirmButton: false,
+    timer: 1500,
+    timerProgressBar: true,
+});
+
+window.toast = toast;
+
+createApp(App).use(router).mount("#app");
